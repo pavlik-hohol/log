6dd8d79dd373b2680f3fd9cb55cfeae7fb059f40
pavlik-hohol <pavlo.shui2006@gmail.com>
2021-04-05 12:53:18 +0300

    nothow

---
+++ gitovka.py
@@ -0,0 +1,14 @@
+def main() :
+    import turtle as tut
+    tut.home()
+    tut.shape('square')
+    tut.color('blue')
+    tut.speed(30)
+    tut.goto(25,50)
+    tut.goto(-25,-50)
+    tut.goto(75,50)
+    tut.goto(-75,-50)
+main()
+
+
+
